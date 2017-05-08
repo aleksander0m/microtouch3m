d5f7919118d7a5ce92955c02430a3ea694912ff4
Aleksander Morgado <aleksander@aleksander.es>
2017-05-08 13:52:59 +0200

    microtouch3m-cli: implement firmware update operation

--- src/microtouch3m-cli/microtouch3m-cli.c
+++ src/microtouch3m-cli/microtouch3m-cli.c
@@ -15,6 +15,7 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include <common.h>
 
@@ -26,6 +27,8 @@
 /******************************************************************************/
 /* Helper: create device based on bus (or first found) */
 
+#define MAX_PORT_NUMBERS 7
+
 static microtouch3m_device_t *
 create_device_by_usb_bus (microtouch3m_context_t *ctx,
                           uint8_t                 bus_number,
@@ -33,6 +36,9 @@ create_device_by_usb_bus (microtouch3m_context_t *ctx,
 {
     microtouch3m_device_t *dev;
     microtouch3m_status_t  st;
+    char                  *location_str;
+    uint8_t                port_numbers[MAX_PORT_NUMBERS];
+    int                    port_numbers_len;
 
     dev = microtouch3m_device_new_by_usb_bus (ctx, bus_number, device_address);
     if (!dev) {
@@ -45,9 +51,53 @@ create_device_by_usb_bus (microtouch3m_context_t *ctx,
         return NULL;
     }
 
-    printf ("microtouch 3m device found at %03u:%03u\n",
+    port_numbers_len = microtouch3m_device_get_usb_location (dev, port_numbers, MAX_PORT_NUMBERS);
+    location_str = str_usb_location (microtouch3m_device_get_usb_bus_number (dev), port_numbers, port_numbers_len);
+
+    printf ("microtouch 3m device found at:\n"
+            "\tbus number:     %u\n"
+            "\tdevice address: %u\n"
+            "\tusb location:   %s\n",
             microtouch3m_device_get_usb_bus_number (dev),
-            microtouch3m_device_get_usb_device_address (dev));
+            microtouch3m_device_get_usb_device_address (dev),
+            location_str);
+
+    free (location_str);
+
+    if ((st = microtouch3m_device_open (dev)) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't open microtouch 3m device: %s\n", microtouch3m_status_to_string (st));
+        microtouch3m_device_unref (dev);
+        return NULL;
+    }
+
+    return dev;
+}
+
+static microtouch3m_device_t *
+create_device_by_usb_location (microtouch3m_context_t *ctx,
+                               uint8_t                 bus_number,
+                               const uint8_t          *port_numbers,
+                               int                     port_numbers_len)
+{
+    microtouch3m_device_t *dev;
+    microtouch3m_status_t  st;
+    char                  *location_str;
+
+    dev = microtouch3m_device_new_by_usb_location (ctx, port_numbers, port_numbers_len);
+    if (!dev)
+        return NULL;
+
+    location_str = str_usb_location (microtouch3m_device_get_usb_bus_number (dev), port_numbers, port_numbers_len);
+
+    printf ("microtouch 3m device found at:\n"
+            "\tbus number:     %u\n"
+            "\tdevice address: %u\n"
+            "\tusb location:   %s\n",
+            microtouch3m_device_get_usb_bus_number (dev),
+            microtouch3m_device_get_usb_device_address (dev),
+            location_str);
+
+    free (location_str);
 
     if ((st = microtouch3m_device_open (dev)) != MICROTOUCH3M_STATUS_OK) {
         fprintf (stderr, "error: couldn't open microtouch 3m device: %s\n", microtouch3m_status_to_string (st));
@@ -133,6 +183,107 @@ out:
 }
 
 /******************************************************************************/
+/* ACTION: firmware update */
+
+#define REBOOT_WAIT_CHECK_RETRIES      30
+#define REBOOT_WAIT_CHECK_TIMEOUT_SECS  2
+
+static int
+run_firmware_update (microtouch3m_context_t *ctx,
+                     uint8_t                 bus_number,
+                     uint8_t                 device_address,
+                     const char             *path)
+{
+    microtouch3m_status_t       st;
+    uint8_t                     buffer[MICROTOUCH3M_FW_IMAGE_SIZE];
+    microtouch3m_device_data_t *dev_data = NULL;
+    microtouch3m_device_t      *dev;
+    int                         ret = EXIT_FAILURE;
+    uint8_t                     real_bus_number;
+    uint8_t                     real_device_address;
+    uint8_t                     port_numbers[MAX_PORT_NUMBERS];
+    int                         port_numbers_len;
+    int                         reboot_wait_check_retries;
+
+    if (!(dev = create_device_by_usb_bus (ctx, bus_number, device_address)))
+        goto out;
+
+    port_numbers_len = microtouch3m_device_get_usb_location (dev, port_numbers, MAX_PORT_NUMBERS);
+
+    if ((st = microtouch3m_firmware_file_read (path, buffer, sizeof (buffer))) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't load firmware file: %s\n", microtouch3m_status_to_string (st));
+        goto out;
+    }
+
+    printf ("backing up device data...\n");
+    if ((st = microtouch3m_device_backup_data (dev, &dev_data)) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't backup device data: %s\n", microtouch3m_status_to_string (st));
+        goto out;
+    }
+
+    printf ("downloading firmware to device EEPROM...\n");
+    if ((st = microtouch3m_device_firmware_update (dev, buffer, sizeof (buffer))) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't download firmware to device EEPROM: %s\n", microtouch3m_status_to_string (st));
+        goto out;
+    }
+
+    /* The device will change address once rebooted, so we'll monitor that as
+     * well to make sure we don't try to use the device before it's rebooted. */
+    real_bus_number     = microtouch3m_device_get_usb_bus_number (dev);
+    real_device_address = microtouch3m_device_get_usb_device_address (dev);
+
+    printf ("rebooting controller...\n");
+    if ((st = microtouch3m_device_reset (dev, MICROTOUCH3M_DEVICE_RESET_REBOOT)) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't reboot controller: %s\n", microtouch3m_status_to_string (st));
+        goto out;
+    }
+
+    /* Forget about the device */
+    microtouch3m_device_close (dev);
+    microtouch3m_device_unref (dev);
+    dev = NULL;
+
+    /* Note: using libusb to wait for the device asynchronously may be more efficient, but really not a big
+     * deal as this operation isn't something you'd be doing often. */
+    for (reboot_wait_check_retries = 0; reboot_wait_check_retries < REBOOT_WAIT_CHECK_RETRIES; reboot_wait_check_retries++) {
+        printf ("[%d/%d] waiting for controller reboot...\n", reboot_wait_check_retries + 1, REBOOT_WAIT_CHECK_RETRIES);
+        sleep (REBOOT_WAIT_CHECK_TIMEOUT_SECS);
+
+        dev = create_device_by_usb_location (ctx, real_bus_number, port_numbers, port_numbers_len);
+
+        /* No device found? */
+        if (!dev)
+            continue;
+        /* Device didn't reboot yet? */
+        if (real_device_address == microtouch3m_device_get_usb_device_address (dev))
+            continue;
+        break;
+    }
+
+    if (!dev) {
+        fprintf (stderr, "error: controller didn't reboot correctly\n");
+        goto out;
+    }
+
+    printf ("restoring device data...\n");
+    if ((st = microtouch3m_device_restore_data (dev, dev_data)) != MICROTOUCH3M_STATUS_OK) {
+        fprintf (stderr, "error: couldn't restore device data: %s\n", microtouch3m_status_to_string (st));
+        goto out;
+    }
+
+    printf ("successfully updated device firmware\n");
+    ret = EXIT_SUCCESS;
+
+out:
+    if (dev) {
+        microtouch3m_device_close (dev);
+        microtouch3m_device_unref (dev);
+    }
+    free (dev_data);
+    return ret;
+}
+
+/******************************************************************************/
 /* Logging */
 
 static pthread_t main_tid;
@@ -164,6 +315,7 @@ print_help (void)
             "Device actions:\n"
             "  -i, --info                       Show device information.\n"
             "  -x, --firmware-dump=[PATH]       Dump firmware to a file.\n"
+            "  -u, --firmware-update=[PATH]     Update firmware in the device.\n"
             "\n"
             "Firmware actions:\n"
             "  -z, --validate-fw-file=[PATH]    Validate firmware file.\n"
@@ -238,6 +390,7 @@ int main (int argc, char **argv)
     bool                    first                     = false;
     bool                    info                      = false;
     char                   *firmware_dump             = NULL;
+    char                   *firmware_update           = NULL;
     char                   *validate_fw_file          = NULL;
     bool                    debug                     = false;
     int                     ret                       = EXIT_FAILURE;
@@ -247,6 +400,7 @@ int main (int argc, char **argv)
         { "first",            no_argument,       0, 'f' },
         { "info",             no_argument,       0, 'i' },
         { "firmware-dump",    required_argument, 0, 'x' },
+        { "firmware-update",  required_argument, 0, 'u' },
         { "validate-fw-file", required_argument, 0, 'z' },
         { "debug",            no_argument,       0, 'd' },
         { "version",          no_argument,       0, 'v' },
@@ -271,6 +425,9 @@ int main (int argc, char **argv)
         case 'x':
             firmware_dump = strdup (optarg);
             break;
+        case 'u':
+            firmware_update = strdup (optarg);
+            break;
         case 'z':
             validate_fw_file = strdup (optarg);
             break;
@@ -287,7 +444,7 @@ int main (int argc, char **argv)
     }
 
     /* Track actions */
-    n_actions_require_device = info + !!firmware_dump;
+    n_actions_require_device = info + !!firmware_dump + !!firmware_update;
     n_actions = !!(validate_fw_file) + n_actions_require_device;
 
     if (n_actions > 1) {
@@ -331,6 +488,8 @@ int main (int argc, char **argv)
         ret = run_info (ctx, bus_number, device_address);
     else if (firmware_dump)
         ret = run_firmware_dump (ctx, bus_number, device_address, firmware_dump);
+    else if (firmware_update)
+        ret = run_firmware_update (ctx, bus_number, device_address, firmware_update);
     else
         assert (0);
 
@@ -339,6 +498,7 @@ out:
 
     free (bus_number_device_address);
     free (firmware_dump);
+    free (firmware_update);
     free (validate_fw_file);
     return ret;
 }
